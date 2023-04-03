f179ce5e6c019e170164911e30613ba22c84cd8e
jejikeh <jejikeh@gmail.com>
2023-04-03 23:37:32 +0300

    WIP: Added authors controller

--- src/Modsen.Authors/Controllers/AuthorsController.cs
+++ src/Modsen.Authors/Controllers/AuthorsController.cs
@@ -1,20 +1,44 @@
 ﻿using AutoMapper;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using Modsen.Authors.Application.Commands.CreateAuthor;
+using Modsen.Authors.Application.Commands.GetAllAuthors;
 using Modsen.Authors.Application.Interfaces;
 
 namespace Modsen.Authors.Controllers;
 
-[Route("api/authors")]
+[Route("api/[controller]")]
 [ApiController]
-public class AuthorController : ControllerBase
+public class AuthorsController : ControllerBase
 {
     private readonly IAuthorRepository _authorRepository;
     private readonly IMapper _mapper;
-
-
-    public AuthorController(IAuthorRepository authorRepository, IMapper mapper)
+    private IMediator? _mediator;
+    private IMediator? Mediator => _mediator ??= HttpContext.RequestServices.GetService<IMediator>();
+    
+    public AuthorsController(IAuthorRepository authorRepository, IMapper mapper)
     {
         _authorRepository = authorRepository;
         _mapper = mapper;
+    }
+
+    [HttpPost]
+    public async Task<ActionResult> Create([FromBody] CreateAuthorCommand createAuthorCommand)
+    {
+        if (Mediator is null)
+            return BadRequest("Internal server error");
+
+        await Mediator.Send(createAuthorCommand);
+        return Ok();
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<AuthorReadMinDto>>> GetAllAuthorsMin()
+    {
+        if (Mediator is null)
+            return BadRequest("Internal server error");
+
+        var authors = await Mediator.Send(new GetAllAuthorsMinQuery());
+        return Ok(authors);
     }
 }
--- src/Modsen.Authors/Program.cs
+++ src/Modsen.Authors/Program.cs
@@ -1,15 +1,10 @@
+using Modsen.Authors.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.RegisterServiceMiddleware();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -21,5 +16,9 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.UseCors("AllowAll");
+
+app.InitializeServiceContextProvider();
 
 app.Run();
