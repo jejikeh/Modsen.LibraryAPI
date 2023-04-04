1b558d7340b1268d66d4736fbc32778b013bc46c
jejikeh <jejikeh@gmail.com>
2023-04-05 01:33:20 +0300

    model layer

--- src/Modsen.Books/Models/Book.cs
+++ src/Modsen.Books/Models/Book.cs
@@ -2,5 +2,11 @@
 
 public class Book
 {
-    
+    public required Guid Id { get; set; }
+    public required string ISBN { get; set; }
+    public required string Title { get; set; }
+    public required string Genre { get; set; }
+    public required string Description { get; set; }
+    public required DateTime Year { get; set; }
+    public required string Author { get; set; }
 }
