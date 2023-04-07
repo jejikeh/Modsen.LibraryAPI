4c85f95d6e931864227bf0e2a64c21f4beb9b2cb
jejikeh <jejikeh@gmail.com>
2023-04-07 13:13:22 +0300

    add new method GetBookByISBN

--- src/Modsen.Books/Application/Interfaces/IBookRepository.cs
+++ src/Modsen.Books/Application/Interfaces/IBookRepository.cs
@@ -7,6 +7,7 @@ public interface IBookRepository
     public Task<IEnumerable<Book>> GetAllBooks();
     public IEnumerable<Book> GetAllAuthorBooks(Guid authorId);
     public Task<Book?> GetBookById(Guid authorId, Guid bookId);
+    public Task<Book?> GetBookByISBN(string isbn);
     public Task CreateBook(Guid authorId, Book book);
     public Task<bool> SaveChangesAsync();
 }
