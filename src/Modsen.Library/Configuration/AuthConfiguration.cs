f8eb07bc4b0e9c0a314c243c89f36f8aae3fca9a
jejikeh <jejikeh@gmail.com>
2023-04-13 15:37:36 +0300

    fix confgiguration class

--- src/Modsen.Library/Configuration/AuthConfiguration.cs
+++ src/Modsen.Library/Configuration/AuthConfiguration.cs
@@ -7,13 +7,11 @@ public static class AuthConfiguration
 {
     public static string Issuer { get; } = "modsen-library";
     public static string Audience { get; } = "library-user";
-    public static string Key { get; } = "TopSecretTokenTopSecretToken1234567890";
     private static string StaticKey { get; } = "TopSecretTokenTopSecretToken1234567890";
+    public static DateTime Expires { get; set; } = DateTime.Now.AddDays(1);
     
     public static SymmetricSecurityKey GetSymmetricSecurityKeyStatic()
     {
         return new SymmetricSecurityKey(Encoding.UTF8.GetBytes(StaticKey));
     }
-
-    public static DateTime Expires { get; set; } = DateTime.Now.AddDays(1);
 }
